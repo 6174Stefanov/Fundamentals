d4a05bf30b495b974a85ec2f0ef722e766b1a19e
ProfessorDumb <114015388+ProfessorDumb@users.noreply.github.com>
2022-09-21 21:47:34 +0300

    Test

---
+++ src/L2DataTypes/E4TownInfo.java
@@ -0,0 +1,15 @@
+package L2DataTypes;
+
+import java.util.Scanner;
+
+public class E4TownInfo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String cityName = scanner.nextLine();
+        int population = Integer.parseInt(scanner.nextLine());
+        short area = Short.parseShort(scanner.nextLine());
+
+        String result = String.format("Town %s has population of %d and area %d square km", cityName, population, area);
+        System.out.println(result);
+    }
+}
